46b98c127426f9eba0195aa0caedadbae4af6add
unknown <Richard@.(none)>
2012-02-03 10:24:46 -0500

    Added comments to the bean class

--- src/main/java/hudson/plugins/selenium/SeleniumRunOptions.java
+++ src/main/java/hudson/plugins/selenium/SeleniumRunOptions.java
@@ -25,18 +25,38 @@ public class SeleniumRunOptions {
 		jvmArgs = new HashMap<String, String>();
 	}
 	
+	/**
+	 * Returns the selenium process arguments 
+	 * 
+	 * @return List of arguments
+	 */
 	public List<String> getSeleniumArguments() {
 		return arguments;
 	}
 	
+	/**
+	 * Returns the jvm arguments that needs to be given to the RcVM class (-D options mostly)
+	 * 
+	 * @return Map of key/value options to be given to the jvm starting selenium
+	 */
 	public Map<String, String> getJVMArguments() {
 		return jvmArgs;
 	}
 	
+	/**
+	 * Add an option to the selenium process' arguments list 
+	 * @param option
+	 */
 	public void addOption(String option) {
 		arguments.add(option);
 	}
 	
+	/**
+	 * Add an option only if the value is set : not null and length > 0
+	 * 
+	 * @param option Option to set
+	 * @param value Value of the option
+	 */
 	public void addOptionIfSet(String option, String value) {
 		if (StringUtils.hasText(value)) {
 			arguments.add(option);
