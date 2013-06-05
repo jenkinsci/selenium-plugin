a3f3ebb189bce632950345379b028c93e8cf9851
Christoph Neuroth <christoph.neuroth@gmail.com>
2013-06-05 15:02:20 +0200

    update to selenium 2.33.0
    the changed install.sh allows building new versions without permission to push
    jars to the jenkins maven repository.

--- install.sh
+++ install.sh
@@ -1,6 +1,22 @@
-version=2.21.0
-#cmd=install:install-file
-cmd=deploy:deploy-file
-mvn $cmd -Dfile=$HOME/Downloads/selenium-server-standalone-${version}.jar \
-    -DgroupId=org.seleniumhq.selenium -DartifactId=selenium-server-standalone -Dversion=${version} -Dpackaging=jar \
-    -DrepositoryId=maven.jenkins-ci.org -Durl=http://maven.jenkins-ci.org:8081/content/repositories/releases 
+version=2.33.0
+
+standaloneFile=$HOME/Downloads/selenium-server-standalone-${version}.jar
+standaloneUrl=https://selenium.googlecode.com/files/selenium-server-standalone-$version.jar
+
+# download the selenium-server-standalone jar if not available
+if [ ! -f $standaloneFile ]; then
+    curl -o $standaloneFile $standaloneUrl
+fi
+
+# Faking local maven repository for selenium-server-standalone dependency.
+mkdir lib/
+mvn org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file \
+    -Dfile=$HOME/Downloads/selenium-server-standalone-${version}.jar \
+    -DgroupId=org.seleniumhq.selenium \
+    -DartifactId=selenium-server-standalone \
+    -Dversion=$version \
+    -Dpackaging=jar
+    -DlocalRepositoryPath=lib/
+
+
+mvn package && echo "Done, packaged plugin to target/selenium.hpi."
