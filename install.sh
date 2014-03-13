73ab446a48f7683d43998e0923cfdb8bfb53057d
Ryan Thomas Correia Ortega <rcorreia@blurb.com>
2014-03-13 13:16:32 -0700

    updating per feedback from darkrift

--- install.sh
+++ install.sh
@@ -1,22 +1,6 @@
-set -e
 version=2.40.0
-
-standaloneFile=/tmp/selenium-server-standalone-${version}.jar
-standaloneUrl=https://selenium.googlecode.com/files/selenium-server-standalone-$version.jar
-
-# download the selenium-server-standalone jar if not available
-if [ ! -f $standaloneFile ]; then
-    curl -o $standaloneFile $standaloneUrl
-fi
-
-# Faking local maven repository for selenium-server-standalone dependency.
-mkdir -p lib/
-mvn org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file \
-    -Dfile=$standaloneFile \
-    -DgroupId=org.seleniumhq.selenium \
-    -DartifactId=selenium-server-standalone \
-    -Dversion=$version \
-    -Dpackaging=jar \
-    -DlocalRepositoryPath=lib/
-
-mvn package && echo "Done, packaged plugin to target/selenium.hpi."
+#cmd=install:install-file
+cmd=deploy:deploy-file
+mvn $cmd -Dfile=$HOME/Downloads/selenium-server-standalone-${version}.jar \
+    -DgroupId=org.seleniumhq.selenium -DartifactId=selenium-server-standalone -Dversion=${version} -Dpackaging=jar \
+    -DrepositoryId=maven.jenkins-ci.org -Durl=http://maven.jenkins-ci.org:8081/content/repositories/releases 
