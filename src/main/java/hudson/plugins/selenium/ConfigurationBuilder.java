66fff84fb90659475501cb7618f53c57b85be1a5
RyanFromQA <8808078+RyanFromQA@users.noreply.github.com>
2019-05-09 01:56:51 -0400

    Update to Selenium 3.141.59 and HtmlUnit 2.33.3 (#138)
    * New unit test for JSON config
    
    * Revert "New unit test for JSON config"
    
    This reverts commit 9ec316abbea41d4234fe3a502e6e1a3b83bfd5e3.
    
    * Bump Selenium to 3.3.1
    
    * Update to Selenium 3.4.0
    
    * Update selenium to 3.13.0
    
    * Bump Version to 3.141.59 and 2.33.3
    
    * Adding configuration to plugins to ignore Selenium's new JDK9-compatible-but-not-required module-info.class files
    
    * Updates to configuration builder due to changes to Selenium Grid's internal classes
    
    * Revert version number

--- src/main/java/hudson/plugins/selenium/ConfigurationBuilder.java
+++ src/main/java/hudson/plugins/selenium/ConfigurationBuilder.java
@@ -6,6 +6,7 @@ import org.openqa.grid.internal.cli.GridHubCliOptions;
 import org.openqa.grid.internal.cli.GridNodeCliOptions;
 import org.openqa.grid.internal.utils.configuration.GridHubConfiguration;
 import org.openqa.grid.internal.utils.configuration.GridNodeConfiguration;
+import org.openqa.grid.selenium.GridLauncherV3;
 
 /**
  * Converts String[] args to Selenium Grid configuration objects using {@link JCommander}
@@ -13,11 +14,15 @@ import org.openqa.grid.internal.utils.configuration.GridNodeConfiguration;
 public class ConfigurationBuilder {
 
 	public static GridNodeConfiguration buildNodeConfig(String[] args) {
-		return new GridNodeCliOptions().parse(args).toConfiguration();
+		GridNodeCliOptions nodeOptions = new GridNodeCliOptions();
+		JCommander.newBuilder().addObject(nodeOptions).build().parse(args);
+		return new GridNodeConfiguration(nodeOptions);
 	}
 
 	public static GridHubConfiguration buildHubConfig(String[] args, Integer port) {
-		GridHubConfiguration config = new GridHubCliOptions().parse(args).toConfiguration();
+		GridHubCliOptions cliOptions = new GridHubCliOptions();
+		JCommander.newBuilder().addObject(cliOptions).build().parse(args);
+		GridHubConfiguration config = new GridHubConfiguration(cliOptions);
 		config.port = port;
 		config.capabilityMatcher = new JenkinsCapabilityMatcher(Channel.current(), config.capabilityMatcher);
 		return config;
